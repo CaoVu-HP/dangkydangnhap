4c639f3d681364260531f8591b03c7648130bb1f
CaoVu-HP <79799971+CaoVu-HP@users.noreply.github.com>
2021-08-11 15:15:56 +0700

    first commit

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subject;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subject=Subject::all();
+        return view('subject',compact('subject'));
+    }
+    public function register($id)
+    {
+        $check = User::where('_id',auth()->user()->_id)->where('subjects.subject_id', $id)->get();
+        if (sizeof($check) != null) {
+            User::where('_id',auth()->user()->_id)->pull('subjects',['subject_id'=>$id]);
+        }else
+            User::where('_id',auth()->user()->_id)->push('subjects', ['subject_id' => $id, 'ts' => time()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subject $subject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subject $subject)
+    {
+        //
+    }
+}
---
+++ resources/views/room.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+    <div class="card-body">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Started Time</th>
+                <th scope="col">Teacher</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @php($i=1)
+            @foreach($subject as $version)
+                <tr>
+
+                    <td>{{$version->name}}</td>
+                    <td>{{$version->teacher}}</td>
+                    <td>{{$version->time}}</td>
+                    <td>
+                        <button type="button" class="btn btn-success"><a href="{{route('register',['id'=>$version->id])}}" class="text-white ">Register</a></button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
